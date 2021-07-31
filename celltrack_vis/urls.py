137f5f6ee99c4521b2c5912c5bab8d150c2f21cb
Hyuhng Min Kim <minkin918@gmail.com>
2021-07-31 16:49:29 +0900

    Add static file urlpattern for static file access

--- celltrack_vis/urls.py
+++ celltrack_vis/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('hyungmin/', views.hyungmin, name='hyungmin'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
