363488966b52d382404eae4ba4ccf3624c7a12bb
wooilkim <wooilkim@korea.ac.kr>
2021-08-11 23:23:34 +0900

    raster to polygon

--- preprocess/read.py
+++ preprocess/read.py
@@ -6,6 +6,13 @@ import gdal
 import copy
 from collections import defaultdict
 import json
+from shapely.geometry import Polygon
+import geopandas as gpd
+import gdal, ogr
+
+import subprocess
+import glob
+import os
 
 
 # # import arcpy
@@ -48,19 +55,61 @@ def test_gdal(path):
                     for i, x in enumerate(y):
                         if x != 0:
                             d[x].append([j, i])
-                poly_objs = dict()
+                # poly_objs = dict()
                 for key in d.keys():
                     tmp = copy.deepcopy(binary)
                     for j, i in d[key]:
                         tmp[j][i] = 1
                     contours, h = cv.findContours(tmp, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-                    poly_objs[int(key)] = [c.tolist() for c in contours]
-                res.append(poly_objs)
+                    # image = cv.drawContours(img, contours, -1, (0, 255, 0), 1)
+                    # poly_objs = []
+
+                    # poly_objs.append(Polygon(np.squeeze(contours[0])))  # 여기
+
+                    polygons = contours[0]
+                    polygons = gpd.GeoDataFrame(polygons, columns=['geometry'])
+                    polygons.to_file('star_polygons.shp')
+
+                    #
+                    # print('c0', contours[0])
+                    # print('c1')
+                    # for c in contours:
+                    #     print(c[0])
+                    # poly_objs[int(key)] = [[int(c[0][0]), int(c[0][1])] for c in contours[0]]
+                # res.append(poly_objs)
             output_file.write(json.dumps(res))
             output_file.flush()
+
+
+def gdal_test(path):
+    in_file = path + '01_RES/mask0000.tif'
+    raster = gdal.Open(in_file)
+    # print(raster.RasterCount)
+    band = raster.GetRasterBand(1)
+    # print(band.GetMaximum())
+    drv = ogr.GetDriverByName('GeoJSON')
+    outfile = drv.CreateDataSource('output.json')
+    outlayer = outfile.CreateLayer('polygonized raster', srs=None)
+    newField = ogr.FieldDefn('DN', ogr.OFTReal)
+    outlayer.CreateField(newField)
+    gdal.Polygonize(band, None, outlayer, 0, [])
+    outfile = None
+    # dst_layername = 'polygonized_test_all_all_2.shp'
+    # drv = ogr.GetDriverByName("ESRI Shapefile")
+    # dst_ds = drv.CreateDataSource(dst_layername)
+    # dst_layer = dst_ds.CreateLayer(dst_layername, srs=in_file)
+    # fd = ogr.FieldDefn("DN", ogr.OFTInteger)
+    # dst_layer.CreateField(fd)
+    # dst_field = dst_layer.GetLayerDefn().GetFieldIndex("DN")
+    # gdal.Polygonize(in_file, None, dst_layer, dst_field, [], callback=None)
+    # out_file = "out.json"
+    # in_file = path + '01_RES/mask0000.tif'
+    # cmdline = ['gdal_polygonize.py', in_file, "-f", "GeoJSON", out_file]
+    # subprocess.call(cmdline)
 
 
 if __name__ == '__main__':
     path = '../celltrack_vis/static/celltrack_vis/data/celltracking_results/BF-C2DL-HSC/'
     # read_tiff(path)
-    test_gdal(path)
+    # test_gdal(path)
+    gdal_test(path)
