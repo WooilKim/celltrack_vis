5e6ebfb0771a87b3bdd61d1cc3598445bafba801
fernok <minkim918@gmail.com>
2021-08-20 18:07:59 +0900

    Update function organize_id

--- preprocess/test.py
+++ preprocess/test.py
@@ -1,5 +1,8 @@
 import tifffile as tiff
 import matplotlib.pyplot as plt
+import csv
+import os
+from pathlib import Path
 
 
 def organize_id(dataset):
@@ -10,6 +13,49 @@ def organize_id(dataset):
     0,"KIT-Sch-GE"
     1,"MU"
     """
+    base_url = 'data/celltracking_results/' + dataset
+    csv_name = '/ids.csv'
+
+    algorithms = []
+    for (_, dirnames, _) in os.walk(base_url):
+        algorithms.extend(dirnames)
+        break
+    
+    ids = base_url + csv_name
+
+    # ids.csv exists
+    if Path(ids).is_file():
+        f = open(ids, 'r')
+        fr = list(csv.reader(f))
+        f.close()
+
+        index = len(fr[1:])
+        current_list = []
+
+        for row in fr[1:]:
+            _, algorithm = row
+            current_list.append(algorithm)
+        
+        difference_list = [i for i in algorithms if i not in current_list]
+
+        f = open(ids, 'a', newline='')
+        fw = csv.writer(f)
+
+        for algorithm in difference_list:
+            fw.writerow([index, algorithm])
+            index += 1
+
+        f.close()
+    # ids.csv does not exist
+    else:
+        f = open(ids, 'w')
+        fw = csv.writer(f)
+        fw.writerow(['id','name'])
+
+        for i, algorithm in enumerate(algorithms):
+            fw.writerow([i, algorithm])
+
+        f.close()
     pass
 
 
@@ -25,14 +71,14 @@ def jaccard(dataset):
 
 
 def test():
-    path1 = 'data/celltracking_results/KIT-Sch-GE/BF-C2DL-HSC/01_RES/mask0000.tif'
-    path2 = 'data/celltracking_results/MU-Lux-CZ/BF-C2DL-HSC/01_RES/mask0000.tif'
+    path1 = 'data/celltracking_results/BF-C2DL-HSC/KIT-Sch-GE/01_RES/mask0000.tif'
+    path2 = 'data/celltracking_results/BF-C2DL-HSC/MU-Lux-CZ/01_RES/mask0000.tif'
 
     a = tiff.imread(path1)
-    for j in a:
-        for i in j:
-            a[j][i] == b[j][i]
-            v+=1
+    # for j in a:
+    #     for i in j:
+    #         a[j][i] == b[j][i]
+    #         v+=1
 
     b = tiff.imread(path1)
     fig = plt.figure()
@@ -44,4 +90,5 @@ def test():
 
 
 if __name__ == '__main__':
+    organize_id('BF-C2DL-HSC')
     test()
