05b5047c0dbdc6cb39630e8bcef5df61b6a51831
wooilkim <wooilkim@korea.ac.kr>
2021-08-20 13:35:32 +0900

    organize todo

--- preprocess/test.py
+++ preprocess/test.py
@@ -4,11 +4,23 @@ import matplotlib.pyplot as plt
 
 def organize_id(dataset):
     # TODO : add new algorithms to ids.csv file (id, name)
+    """
+
+    id,name
+    0,"KIT-Sch-GE"
+    1,"MU"
+    """
     pass
 
 
 def jaccard(dataset):
     # TODO : wrtie {id_a}_{id_b}.csv (t, sim) between existing algorithms (id_a) and new algorithm (id_b)
+    """
+
+    0_1.csv
+    0, 30,
+    0,
+    """
     pass
 
 
@@ -17,6 +29,11 @@ def test():
     path2 = 'data/celltracking_results/MU-Lux-CZ/BF-C2DL-HSC/01_RES/mask0000.tif'
 
     a = tiff.imread(path1)
+    for j in a:
+        for i in j:
+            a[j][i] == b[j][i]
+            v+=1
+
     b = tiff.imread(path1)
     fig = plt.figure()
     fig.add_subplot(1, 2, 1)
